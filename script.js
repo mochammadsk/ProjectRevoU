fef68d34592cf0d9caae3fd06523a2cd31462f45
Mochammadsk <mochammadsk99@gmail.com>
2023-05-27 01:22:32 +0800

    update smooth efect and navbar fix

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
 function validateForm(event) {
-    event.preventDefault();
-  
-    var nameInput = document.getElementById('name');
-    var emailInput = document.getElementById('email');
-    var optionInput = document.getElementById('option');
+  event.preventDefault();
 
-    var name = nameInput.value;
-    var email = emailInput.value;
-    var option = optionInput.value;
-  
-    if(name == ''){
-      alert('Nama Tidak Boleh Kosong');
-      return false;
-    }
-  
-    if(email == ''){
-      alert('Email Tidak Boleh Kosong');
-      return false;
-    }
-  
-    if(!isValidEmail(email)){
-      alert('Email Tidak Valid');
-      return false;
-    }
+  var nameInput = document.getElementById("name");
+  var emailInput = document.getElementById("email");
+  var optionInput = document.getElementById("option");
 
-    if(option == ''){
-      alert('Anda Belum Memilih');
-      return false;
-    }
-    
-    alert('Berhasil Terkirim');
+  var name = nameInput.value;
+  var email = emailInput.value;
+  var option = optionInput.value;
+
+  if (name == "") {
+    alert("Nama Tidak Boleh Kosong");
+    return false;
+  }
+
+  if (email == "") {
+    alert("Email Tidak Boleh Kosong");
+    return false;
+  }
+
+  if (!isValidEmail(email)) {
+    alert("Email Tidak Valid");
+    return false;
+  }
+
+  if (option == "") {
+    alert("Anda Belum Memilih");
+    return false;
+  }
+
+  alert("Berhasil Terkirim");
 }
-  
+
 function isValidEmail(email) {
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
+
+var navbarLinks = document.querySelectorAll("button a");
+
+// Menambahkan event listener pada setiap link
+navbarLinks.forEach(function (link) {
+  link.addEventListener("click", smoothScroll);
+});
+
+// Fungsi untuk scroll halus
+function smoothScroll(event) {
+  event.preventDefault();
+
+  // Mendapatkan target dari link yang diklik
+  var targetId = this.getAttribute("href");
+
+  // Mendapatkan elemen target
+  var targetElement = document.querySelector(targetId);
+
+  // Menggunakan scrollIntoView dengan opsi smooth untuk scroll halus
+  targetElement.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
+
+// function showNextImage() {
+//   var sliderImages = document.querySelectorAll(".slider img");
+//   var currentImageIndex = 0;
+
+//   sliderImages[currentImageIndex].classList.remove("active");
+//   currentImageIndex = (currentImageIndex + 1) % sliderImages.length;
+//   sliderImages[currentImageIndex].classList.add("active");
+// }
+// setInterval(showNextImage, 3000);
